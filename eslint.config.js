2fad7138310d51eed70aac3e55a88829a5e7fdc7
fyzanshaik <fyzan.shaik@gmail.com>
2025-10-12 23:13:25 +0530

    initialize project
    Signed-off-by: fyzanshaik <fyzan.shaik@gmail.com>

---
+++ eslint.config.js
@@ -0,0 +1,58 @@
+import js from '@eslint/js';
+import typescript from '@typescript-eslint/eslint-plugin';
+import typescriptParser from '@typescript-eslint/parser';
+import astro from 'eslint-plugin-astro';
+import prettier from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.{js,mjs,cjs,ts,tsx}'],
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': typescript,
+      prettier: prettier,
+    },
+    rules: {
+      ...typescript.configs.recommended.rules,
+      ...prettierConfig.rules,
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_' },
+      ],
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'no-console': 'warn',
+    },
+  },
+  ...astro.configs['flat/recommended'],
+  {
+    files: ['**/*.astro'],
+    languageOptions: {
+      parser: astro.parser,
+      parserOptions: {
+        parser: typescriptParser,
+        extraFileExtensions: ['.astro'],
+      },
+    },
+    rules: {
+      ...astro.configs.recommended.rules,
+    },
+  },
+  {
+    ignores: [
+      'node_modules/',
+      'dist/',
+      '.astro/',
+      '*.config.js',
+      '*.config.mjs',
+    ],
+  },
+];
